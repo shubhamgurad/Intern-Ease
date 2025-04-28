dd2eca989091603e718b02706d9ebcaa5514a95d
Shubhamgurad <guradshubham96@gmail.com>
2025-04-29 01:13:02 +0530

    Updated Signupform.jsx and added project zip

--- myproject/src/Signupfrom.jsx
+++ myproject/src/Signupfrom.jsx
@@ -87,7 +87,6 @@ const SignUpForm = () => {
                     name="course"
                     value={course}
                     placeholder='Select Course'
-                    // onChange={(e) => setCourse(e.target.value)}
                     required
                   ><i class="login__icon fa-solid fa-circle-chevron-down signup_icons"></i>
                     <option value="course">Select Course</option>
@@ -106,7 +105,6 @@ const SignUpForm = () => {
                     name="photo"
                     placeholder="Upload your photo with unifrom"
                     accept="image/*"
-                    // onChange={(e) => setPhoto(e.target.files[0])}
                     required
                   />
                 </div>
